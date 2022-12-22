32c67ce6d5d74e28467f1c875ca4c5fe0a999d30
Shafaat Jamil <109018243+consis-tency@users.noreply.github.com>
2022-12-22 14:06:07 +0600

    Add Homework Directory

---
+++ Alternating LEDs/Alternating LEDs.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); //baud rate of the serial communication between the board and pc
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);
+  Serial.println("Pin13 ON  -- Pin12 OFF"); // Print the line to serial monitor of the pc
+  delay(500);
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  Serial.println("Pin13 OFF -- Pin12 ON"); // Print the line to serial monitor of the pc
+  delay(500);
+}
