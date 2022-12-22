0a47b86981a8287fae93232bbbafe44aaf2f882e
Shafaat Jamil <109018243+consis-tency@users.noreply.github.com>
2022-12-22 14:00:54 +0600

    Added output checking for serial monitors

--- Alternating LEDs.ino
+++ Alternating LEDs.ino
@@ -1,5 +1,6 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600); //baud rate of the serial communication between the board and pc
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
 
@@ -9,8 +10,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(13, HIGH);
   digitalWrite(12, LOW);
+  println("Pin13 ON  -- Pin12 OFF"); // Print the line to serial monitor of the pc
   delay(500);
   digitalWrite(13, LOW);
   digitalWrite(12, HIGH);
+  println("Pin13 OFF -- Pin12 ON"); // Print the line to serial monitor of the pc
   delay(500);
 }
